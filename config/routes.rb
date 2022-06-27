d794c8ea4f61b81b4cb42c103618d217d73f3c51
Dagi <dagi.zewdu.dz@gmail.com>
2022-06-27 17:26:26 +0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get '/user/sign_out' => 'devise/sessions#destroy'
+  end
+  resources :transacs
+  resources :categories
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "static_pages#splash"
 end
