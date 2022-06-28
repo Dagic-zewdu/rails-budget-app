9ba2cd5cab79098e538a3b107ce2578c03431ec2
Dagi <dagi.zewdu.dz@gmail.com>
2022-06-28 22:09:49 +0300

    Added categories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user, only: %i[destroy]
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
